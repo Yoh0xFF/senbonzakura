2789c4b9b5727026945101d5c9c14801f63e1fd0
Ioram Gordadze <Yoh0xFF@gmail.com>
2024-11-01 05:04:08 +0400

    Add interior mutability

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,15 +1,27 @@
+use std::cell::Cell;
+
 #[derive(Debug, Clone)]
 pub struct Lexer {
-    index: u32,
+    index: Cell<u32>,
 }
 
 impl Lexer {
     pub fn new() -> Self {
-        Lexer { index: 0 }
+        Lexer {
+            index: Cell::new(0),
+        }
     }
 
-    pub fn next_token(&mut self) -> String {
-        self.index += 1;
+    pub fn next_token(&self) -> String {
+        self.increment();
         return String::from("Hello World");
     }
+
+    fn increment(&self) {
+        self.index.set(self.index.get() + 1);
+    }
+
+    // fn index(&self) -> u32 {
+    //     return self.index.get();
+    // }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use crate::lexer::Lexer;
 mod lexer;
 
 fn main() {
-    let mut lexer = Lexer::new();
+    let lexer = Lexer::new();
     let next_token = lexer.next_token();
     println!("Token: {}", next_token);
 }
