e8c4db8b414d27760f1253e638713729daadf65c
Ioram Gordadze <Yoh0xFF@gmail.com>
2025-03-07 23:58:41 +0400

    Add more unit tests

--- src/parser/tests/parser_test_expressions.rs
+++ src/parser/tests/parser_test_expressions.rs
@@ -37,3 +37,58 @@ fn test_multiple_literal() {
         "#,
     )
 }
+
+#[test]
+fn test_binary_expression_with_additive_operator() {
+    execute(
+        "1 + 2;",
+        r#"
+        (program
+            (expr (binary "+" (number 1) (number 2))))
+        "#,
+    )
+}
+
+#[test]
+fn test_binary_expression_with_multiple_additive_operators() {
+    execute(
+        "1 + 2 + 3;",
+        r#"
+        (program
+            (expr (binary "+" (binary "+" (number 1) (number 2)) (number 3))))
+        "#,
+    )
+}
+
+#[test]
+fn test_binary_expression_with_multiple_additive_operators_and_parentheses() {
+    execute(
+        "1 + (2 + 3);",
+        r#"
+        (program
+            (expr (binary "+" (number 1) (binary "+" (number 2) (number 3)))))
+        "#,
+    )
+}
+
+#[test]
+fn test_binary_expression_with_multiple_additive_and_factor_operators() {
+    execute(
+        "1 + 2 * 3;",
+        r#"
+        (program
+            (expr (binary "+" (number 1) (binary "*" (number 2) (number 3)))))
+        "#,
+    )
+}
+
+#[test]
+fn test_binary_expression_with_multiple_additive_and_factor_operators_and_parentheses() {
+    execute(
+        "(1 + 2) * 3;",
+        r#"
+        (program
+            (expr (binary "*" (binary "+" (number 1) (number 2)) (number 3))))
+        "#,
+    )
+}
