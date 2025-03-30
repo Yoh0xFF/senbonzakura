a12f23f00b29c2153c959952de9589f4b4582e05
Ioram Gordadze <Yoh0xFF@gmail.com>
2025-03-30 18:02:33 +0400

    Rename files

--- src/parser/tests/mod.rs
+++ src/parser/tests/mod.rs
@@ -1,26 +1,18 @@
 #[cfg(test)]
 mod parser_tests;
-
 #[cfg(test)]
-mod test_additive_and_factor_operator_expressions;
-
+mod test_binary_additive_and_factor_operator_expressions;
 #[cfg(test)]
-mod test_assignment_expressions;
-
+mod test_binary_equality_expressions;
+#[cfg(test)]
+mod test_binary_relational_operator_expressions;
 #[cfg(test)]
 mod test_block_statements;
-
 #[cfg(test)]
-mod test_equality_expressions;
-
-#[cfg(test)]
-mod test_if_statements;
-
+mod test_conditional_if_statements;
 #[cfg(test)]
 mod test_literal_expressions;
-
 #[cfg(test)]
-mod test_relational_operator_expressions;
-
+mod test_variable_assignment_expressions;
 #[cfg(test)]
 mod test_variable_declaration_statements;
