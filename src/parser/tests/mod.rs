13db55e3d3f1114b445522c1eefdd3ce83c63120
Ioram Gordadze <Yoh0xFF@gmail.com>
2025-05-20 13:32:06 +0400

    Move literal expression unit tests to yaml

--- src/parser/tests/mod.rs
+++ src/parser/tests/mod.rs
@@ -5,8 +5,6 @@ mod test_expression_parse_binary;
 #[cfg(test)]
 mod test_expression_parse_left_hand_side;
 #[cfg(test)]
-mod test_expression_parse_literals;
-#[cfg(test)]
 mod test_expression_parse_primary;
 #[cfg(test)]
 mod test_expression_parse_relational_and_logical;
@@ -23,6 +21,6 @@ mod test_statement_parse_function_declaration;
 #[cfg(test)]
 mod test_statement_parse_loop;
 #[cfg(test)]
-mod test_statement_parse_variable_declaration;
+mod test_yaml_files;
 #[cfg(test)]
 mod utils;
--- src/parser/tests/test_statement_parse_variable_declaration.rs
+++
@@ -1,6 +0,0 @@
-use super::utils::execute_yaml_test_cases;
-
-#[test]
-fn test_statement_parse_variable_declaration() {
-    execute_yaml_test_cases("tests/parser/variable_declaration.yaml");
-}
