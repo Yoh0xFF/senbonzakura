7171a673e529d3f3543617702f75457f52957267
Ioram Gordadze <Yoh0xFF@gmail.com>
2025-05-23 10:20:22 +0400

    Move lexer unit tests to yaml format

--- src/lexer/tests/mod.rs
+++ src/lexer/tests/mod.rs
@@ -2,10 +2,4 @@
 mod internal_util;
 
 #[cfg(test)]
-mod lexer_test_numbers;
-
-#[cfg(test)]
-mod lexer_test_strings;
-
-#[cfg(test)]
 mod test_yaml_files;
--- src/lexer/tests/test_yaml_files.rs
+++ src/lexer/tests/test_yaml_files.rs
@@ -4,3 +4,13 @@ use super::internal_util::execute_yaml_test_cases;
 fn test_lexer_parse_comments() {
     execute_yaml_test_cases("tests/lexer/test_comment_tokens.yaml");
 }
+
+#[test]
+fn test_number_tokens() {
+    execute_yaml_test_cases("tests/lexer/test_number_tokens.yaml");
+}
+
+#[test]
+fn test_string_tokens() {
+    execute_yaml_test_cases("tests/lexer/test_string_tokens.yaml");
+}
